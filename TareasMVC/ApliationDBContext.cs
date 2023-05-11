619732a5434ddf668af3941b3086e7415c996e95
dann9422 <dannsk82294@gmail.com>
2023-05-11 09:27:39 -0600

    Agregar archivos de proyecto.

---
+++ TareasMVC/ApliationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TareasMVC
+{
+    public class ApliationDBContext : DbContext
+    {
+        public ApliationDBContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+    }
+}
